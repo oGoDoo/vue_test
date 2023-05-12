83d9203f7046f35b1b66b2249a9db680d8f9e81b
qiaosl <508050289@qq.com>
2023-05-12 09:39:44 +0800

    5/12

---
+++ 04_src_mixin混入(合）/components/School.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- 组件的结构 -->
+    <div>
+        <h2 @click="showName">学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+    </div>
+</template>
+
+<script>
+import {mixin,hunhe} from '../mixin'
+    export default {
+    name:'School',
+    data() {
+        return {
+            name: '万合技校',
+            address: '安徽四川',
+        }
+    },
+    mixins:[mixin,hunhe]
+}
+</script>
