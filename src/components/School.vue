afabb20640d9e45ed8e34935020f0e58100091b8
qiaosl <508050289@qq.com>
2023-07-18 15:21:15 +0800

    09_src_TodoList_本地存储

---
+++ src/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="school">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'School',
+    data() {
+        return {
+            name: '万合技校',
+            address: '安徽四川',
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+    .school{
+        background-color: skyblue;
+        padding: 5px;
+    }
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="Student">
+        <h2>学生名称：{{name}}</h2>
+        <h2>学生性别：{{sex}}</h2>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'Student',
+        data(){
+            return{
+                name:'乔双龙',
+                sex:'男'
+            }
+        }
+    }
+    
+</script>
+
+<style scoped  lang="less">
+    .Student{
+        background-color: pink;
+        padding: 5px;
+        margin-top: 30px;
+    }
+</style> 
