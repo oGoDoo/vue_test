2300a3e1715d38859ad9362c15b116b5b5986e8d
qiaosl <508050289@qq.com>
2023-08-10 15:05:09 +0800

    过度和动画

---
+++ 17_src_过度与动画/components/Test.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">显示隐藏</button>
+        <Transition name = "hello" appear>
+            <h1 v-show="isShow" class="come">你好啊！</h1>
+        </Transition>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'Test',
+        data(){
+            return {
+                isShow:true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1{
+        background-color: orange;
+    }
+
+    .hello-enter-active{
+        animation: whjx 1s linear; /* linear 平滑 */
+    }
+    .hello-leave-active{
+        animation: whjx 1s linear reverse;
+    }
+
+    @keyframes whjx{
+        from{
+            transform: translateX(-100%)
+        }
+        to{
+            transform: translateX(0px);
+        }
+    } 
+</style>
