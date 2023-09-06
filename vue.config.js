93ee7698b06eabce8992e223938ebd945213d51a
qiaosl <508050289@qq.com>
2023-09-06 19:02:42 +0800

    烟草提交

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,20 @@ module.exports = {
       entry: 'src/main.js',
     }
   },
-  lintOnSave:false //关闭语法检查
+  lintOnSave:false, //关闭语法检查
+  //开启代理服务器（方式一）
+  /* devServer: {
+    proxy: 'http://localhost:5000'
+  }, */
+
+  //开启代理服务器（方式一）
+  devServer: {
+    proxy: {
+      '/qsl': {
+        target: 'http://localhost:5000/student',
+        // ws: true,
+        // changeOrigin: true
+      }
+    }
+  }
 }
